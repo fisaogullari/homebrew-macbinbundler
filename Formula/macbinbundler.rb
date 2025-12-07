02f87b1c3bd602b302a956b04ae96d43ffcf6665
Fatih Isaogullari <fisaogullari@gmail.com>
2025-12-07 21:17:27 +0300

    add homebrew formula

---
+++ Formula/macbinbundler.rb
@@ -0,0 +1,11 @@
+class MyApp < Formula
+  desc "A simple and humble cli app bundles executables and dynamic libraries on mac for portability. "
+  homepage "https://github.com/fisaogullari/macbinbundler"
+  head "https://github.com/fisaogullari/macbinbundler.git", branch: "main"
+  license "MIT"
+
+  depends_on "rust" => :build
+    def install
+      system "cargo", "install", "--locked", "--root", prefix
+    end
+  end
